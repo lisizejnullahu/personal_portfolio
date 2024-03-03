b15ff4b8e0aa7f82aeded026cdfb092bd4ec5ce3
lisichupo <118574316+kemptor@users.noreply.github.com>
2024-03-03 16:22:27 -0500

    almost done

--- src/components/canvas/index.ts
+++
@@ -1,6 +0,0 @@
-import EarthCanvas from './Earth'
-import BallCanvas from './Ball'
-import ComputersCanvas from './Computer'
-import StarsCanvas from './Stars'
-
-export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas }
--- src/maath-random.esm.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function inSphere(
-  array: Float32Array,
-  options: { radius: number }
-): Float32Array // Update the return type to Float32Array
